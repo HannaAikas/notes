cdc2849164d5c374c2d02635c46a770fba51262f
Hanna <hannaaikas@gmail.com>
2019-09-03 11:00:18 +0100

    Refactor: Wrap assert in a module pattern

--- spec/assert.js
+++ spec/assert.js
@@ -1,9 +1,12 @@
-var assert = {
-  isTrue: function(assertionToCheck) {
-    if (!assertionToCheck) {
-      throw new Error("Assertion failed: " + assertionToCheck + " is not truthy");
-    } else {
-      console.log("Assertion passed: " + assertionToCheck)
+(function(exports) {
+  var assert = {
+    isTrue: function(assertionToCheck) {
+      if (!assertionToCheck) {
+        throw new Error("Assertion failed: " + assertionToCheck + " is not truthy");
+      } else {
+        console.log("Assertion passed: " + assertionToCheck)
+      }
     }
-  }
-};
+  };
+  exports.assert = assert;
+})(this);
