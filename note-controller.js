9c516d4d565e4a6bde006ab949c191fe9d7de6d2
Chuan Cheng <c_chuan23@hotmail.com>
2019-09-04 15:34:21 +0100

    spike add note-controller

--- note-controller.js
+++ note-controller.js
@@ -1,4 +1,23 @@
-console.log(document.getElementById('app'));
+// console.log(document.getElementById('app'));
+//
+// var greeting = document.getElementById('app');
+// greeting.innerText = 'howdy';
 
-var greeting = document.getElementById('app');
-greeting.innerText = 'howdy';
+
+(function(exports){
+  function NoteController(noteList){
+    this.noteList = noteList;
+    this.noteList.createNewNote("Favourite drink: seltzer");
+
+    this.noteListView = new NoteListView(this.noteList);
+
+    NoteController.prototype.display = function(){
+      var longString = this.noteListView.displayAllNotes();
+      console.log(longString);
+      document.getElementById("app").innerHTML = longString;
+    }
+  }
+
+  exports.NoteController = NoteController;
+
+})(this);
